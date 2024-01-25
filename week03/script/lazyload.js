29a8b19605b9c679a998de1882e8e96eb0579990
nwangs-p <nwa23004@byui.edu>
2024-01-25 02:00:45 -0800

    week03 works

---
+++ week03/script/lazyload.js
@@ -0,0 +1,8 @@
+const currentYear = new Date().getFullYear();
+const yearElement = document.getElementById("year");
+yearElement.textContent = currentYear;
+
+// Get the last modified date and set it to the "lastModified" element
+const lastModifiedDate = new Date(document.lastModified).toLocaleString();
+const lastModifiedElement = document.getElementById("lastModified");
+lastModifiedElement.textContent = "Last Modified: " + lastModifiedDate;
